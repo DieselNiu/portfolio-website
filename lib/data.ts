12f9d58b15d5b89fd1335dd41a01bcb57f65f239
DieselNiu <niu853273494@qq.com>
2024-04-08 22:29:45 +0800

    done: update info

--- lib/data.ts
+++ lib/data.ts
@@ -35,16 +35,16 @@ export const links = [
 
 export const experiencesData = [
   {
-    title: "Graduated bootcamp",
-    location: "Miami, FL",
+    title: "back-end developer",
+    location: "Chengdu, China",
     description:
-      "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
+      "I graduated after 4 years studying in university . I immediately found a job as a back-end developer.",
     icon: React.createElement(LuGraduationCap),
-    date: "2019",
+    date: "2018",
   },
   {
-    title: "Front-End Developer",
-    location: "Orlando, FL",
+    title: "front-end developer",
+    location: "Shanghai, China",
     description:
       "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
     icon: React.createElement(CgWorkAlt),
@@ -52,11 +52,11 @@ export const experiencesData = [
   },
   {
     title: "Full-Stack Developer",
-    location: "Houston, TX",
+    location: "Zhengzhou, China",
     description:
       "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
     icon: React.createElement(FaReact),
-    date: "2021 - present",
+    date: "2023 - present",
   },
 ] as const;
 
